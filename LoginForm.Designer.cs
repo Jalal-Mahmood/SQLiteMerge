a039286e5bf7921fe93d611c344a288c2dc164cb
Jalal Mahmood <jalal.mahmood@yahoo.com>
2024-02-21 21:45:16 -0500

    Add files via upload

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace BioMid
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblEnglish = new System.Windows.Forms.Label();
+            this.lblPashto = new System.Windows.Forms.Label();
+            this.lblDari = new System.Windows.Forms.Label();
+            this.lblSettings = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblEmail.Location = new System.Drawing.Point(27, 192);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(48, 20);
+            this.lblEmail.TabIndex = 1;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(31, 215);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(244, 26);
+            this.txtEmail.TabIndex = 0;
+            this.txtEmail.Text = "admin@gmail.com";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(44)))), ((int)(((byte)(66)))));
+            this.btnLogin.Image = ((System.Drawing.Image)(resources.GetObject("btnLogin.Image")));
+            this.btnLogin.Location = new System.Drawing.Point(31, 394);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(244, 38);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLogin.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(31, 276);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(244, 26);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.Text = "xyz@123";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblPassword.Location = new System.Drawing.Point(27, 253);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(78, 20);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblError
+            // 
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Tomato;
+            this.lblError.Location = new System.Drawing.Point(28, 305);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(247, 52);
+            this.lblError.TabIndex = 6;
+            // 
+            // btnClose
+            // 
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.Transparent;
+            this.btnClose.Image = global::BioMid.Properties.Resources.Bad;
+            this.btnClose.Location = new System.Drawing.Point(280, 1);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(25, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BioMid.Properties.Resources.biomid_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(54, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 77);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblEnglish
+            // 
+            this.lblEnglish.AutoSize = true;
+            this.lblEnglish.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblEnglish.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnglish.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(44)))), ((int)(((byte)(66)))));
+            this.lblEnglish.Location = new System.Drawing.Point(92, 496);
+            this.lblEnglish.Name = "lblEnglish";
+            this.lblEnglish.Size = new System.Drawing.Size(41, 13);
+            this.lblEnglish.TabIndex = 7;
+            this.lblEnglish.Text = "English";
+            this.lblEnglish.Click += new System.EventHandler(this.lblEnglish_Click);
+            // 
+            // lblPashto
+            // 
+            this.lblPashto.AutoSize = true;
+            this.lblPashto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblPashto.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPashto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(44)))), ((int)(((byte)(66)))));
+            this.lblPashto.Location = new System.Drawing.Point(144, 496);
+            this.lblPashto.Name = "lblPashto";
+            this.lblPashto.Size = new System.Drawing.Size(30, 13);
+            this.lblPashto.TabIndex = 8;
+            this.lblPashto.Text = "پښتو";
+            this.lblPashto.Click += new System.EventHandler(this.lblPashto_Click);
+            // 
+            // lblDari
+            // 
+            this.lblDari.AutoSize = true;
+            this.lblDari.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblDari.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDari.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(44)))), ((int)(((byte)(66)))));
+            this.lblDari.Location = new System.Drawing.Point(185, 496);
+            this.lblDari.Name = "lblDari";
+            this.lblDari.Size = new System.Drawing.Size(27, 13);
+            this.lblDari.TabIndex = 9;
+            this.lblDari.Text = "دري";
+            this.lblDari.Click += new System.EventHandler(this.lblDari_Click);
+            // 
+            // lblSettings
+            // 
+            this.lblSettings.AutoSize = true;
+            this.lblSettings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblSettings.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSettings.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(44)))), ((int)(((byte)(66)))));
+            this.lblSettings.Location = new System.Drawing.Point(12, 496);
+            this.lblSettings.Name = "lblSettings";
+            this.lblSettings.Size = new System.Drawing.Size(45, 13);
+            this.lblSettings.TabIndex = 10;
+            this.lblSettings.Text = "Settings";
+            this.lblSettings.Click += new System.EventHandler(this.lblSettings_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(96)))), ((int)(((byte)(122)))));
+            this.ClientSize = new System.Drawing.Size(306, 518);
+            this.Controls.Add(this.lblSettings);
+            this.Controls.Add(this.lblDari);
+            this.Controls.Add(this.lblPashto);
+            this.Controls.Add(this.lblEnglish);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LoginForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LoginForm_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblEnglish;
+        private System.Windows.Forms.Label lblPashto;
+        private System.Windows.Forms.Label lblDari;
+        private System.Windows.Forms.Label lblSettings;
+    }
+}
+
---
+++ LoginForm.cs
@@ -0,0 +1,193 @@
+﻿using BioMid.Forms;
+using BioMid.Models;
+using BioMid.Properties;
+using BioMid.SQLite;
+using BioMid.Utility;
+using IARCSC.Models;
+using IARCSC.Utility;
+using Neurotec.Biometrics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BioMid
+{
+    public partial class LoginForm : Form
+    {
+        private Point _mouseLoc;
+        public static ResourceManager rmLocale;
+
+        // List<User> users;
+        public HttpClient client;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+            //setLocale("en_locale");
+            client = new HttpClient();
+            setLocale(Settings.Default.locale);
+        }
+
+
+
+
+        private void LoginForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            _mouseLoc = e.Location;
+
+        }
+
+        private void LoginForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int dx = e.Location.X - _mouseLoc.X;
+                int dy = e.Location.Y - _mouseLoc.Y;
+                this.Location = new Point(this.Location.X + dx, this.Location.Y + dy);
+            }
+        }
+
+
+
+        public void Alert(string msg, AlertForm.enumType type)
+        {
+            AlertForm frm = new AlertForm();
+            frm.showAlert(msg, type);
+        }
+
+
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+
+
+        private void lblPashto_Click(object sender, EventArgs e)
+        {
+            updateLocalSetting("pa_locale");
+        }
+
+        private void lblEnglish_Click(object sender, EventArgs e)
+        {
+            updateLocalSetting("en_locale");
+
+        }
+
+        private void lblDari_Click(object sender, EventArgs e)
+        {
+            updateLocalSetting("dr_locale");
+        }
+
+
+
+        private void updateLocalSetting(string locale)
+        {
+            Settings.Default.locale = locale;
+            Settings.Default.Save();
+
+            setLocale(Settings.Default.locale);
+
+        }
+
+
+
+        private void setLocale(string locale)
+        {
+            rmLocale = new ResourceManager("BioMid." + locale, Assembly.GetExecutingAssembly());
+
+            lblEmail.Text = rmLocale.GetString("email");
+            lblPassword.Text = rmLocale.GetString("password");
+            btnLogin.Text = rmLocale.GetString("login");
+
+        }
+
+
+        private void lblSettings_Click(object sender, EventArgs e)
+        {
+            SettingForm sf = new SettingForm();
+            sf.ShowDialog();
+        }
+
+        private bool ValidateLogin()
+        {
+            bool result = true;
+            if (string.IsNullOrEmpty(txtEmail.Text) || string.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                txtEmail.BackColor = Color.Tomato;
+                result = true;
+            }
+            else
+            {
+                txtEmail.BackColor = Color.White;
+            }
+
+            if (string.IsNullOrEmpty(txtPassword.Text) || string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                txtPassword.BackColor = Color.Tomato;
+                result = true;
+            }
+            else
+            {
+                txtPassword.BackColor = Color.White;
+            }
+
+            return result;
+        }
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            //client.Credentials = CredentialCache.DefaultNetworkCredentials;
+            btnLogin.Enabled = false;
+            //bool isSuccess = await performLogin();
+            if (ValidateLogin())
+            {
+                LoginCredentials loginCredentials = new LoginCredentials();
+
+                loginCredentials.email = txtEmail.Text;
+                loginCredentials.password = txtPassword.Text;
+                loginCredentials.appName = "biomid";
+
+                LoginData loginData = await User.Login(loginCredentials);
+
+                //if (loginData.user.usertype != "admin" || loginData.user.usertype != "superadmin" || loginData.user.appName != "biomid")
+                //{
+                //    Alert("You are unauthorized: ", AlertForm.enumType.Error);
+                //    //return;
+                //}
+                //bool status = await performLogin(txtEmail.Text, txtPassword.Text);
+                if (loginData.HttpResponse.IsSuccessStatusCode)
+                {
+                    Environment.SetEnvironmentVariable("access_token", loginData.token);
+                    Environment.SetEnvironmentVariable("token_type", loginData.token_type);
+                    Environment.SetEnvironmentVariable("expires_in", loginData.expires_in);
+                    Environment.SetEnvironmentVariable("user_id", loginData.user.Id.ToString());
+                    Environment.SetEnvironmentVariable("user_name", loginData.user.username.ToString());
+                    Environment.SetEnvironmentVariable("user_email", loginData.user.email.ToString());
+                    this.DialogResult = DialogResult.OK;
+                  //  MainForm mf = new MainForm();
+                    //mf.Show();
+                    //this.Hide();
+                }
+                else
+                {
+                    Alert("Cant login to the system: " + loginData.HttpResponse.ReasonPhrase, AlertForm.enumType.Error);
+                }
+            }
+
+            btnLogin.Enabled = true;
+        }
+    }
+}
